0f3fd9412519ae3b64a00f9f8bdc7beaf052ce55
chiranjivi9 <jawalechiranjivi@gmail.com>
2022-10-20 13:25:02 -0400

    Add a standard api function.

--- src/brigdeApis.ts
+++ src/brigdeApis.ts
@@ -27,9 +27,46 @@ type Yuv444pInput = {
   pixel_format: string;
 };
 
+type BridgeApiInput = {
+  method: string,
+  path: string,
+  parameters: {}
+}
+
+interface Error {
+  isSuccess: boolean;
+  message: string;
+  error: {}
+}
+
+const sendError = (err: Error) => {
+  console.error(err);
+  let error: Error = { isSuccess: err.isSuccess, message: err.message, error: err };
+  return error
+}
+
+
+const bridgeApi = (args: BridgeApiInput) => {
+
+  axios({
+    method: (args.method).toUpperCase(),
+    url: `${BASE_URI}/${args.path}`,
+    data: args.parameters
+  })
+    .then((response) => {
+      return { isSuccess: true, response: response.data };
+    })
+    .catch((error) => {
+      console.log("Error: ", error);
+      return { isSuccess: false, error };
+    })
+}
+
+
 export async function isBridgeRunning() {
+  console.log("Checking if Bridge is running...");
+
   try {
-    console.log("Checking if Bridge is running...");
     let response = await axios.get(BASE_URI);
     if (response.status === 200) {
       console.log("Bridge is running...");
@@ -38,96 +75,77 @@ export async function isBridgeRunning() {
 
     console.log("Bridge is NOT running...");
     return { isSuccess: false, message: "Bridge is NOT running..." };
+
   } catch (error: any) {
     console.log("Error in checking Bridge status: " + error);
     return { isSuccess: false, message: "Error in checking Bridge status: " };
   }
 }
 
+
 export async function playContent(args: PlayInput) {
-  try {
-    // validation
-    if (!args.looking_glass_device_index || !args.source) {
-      return { isSuccess: false, message: "Missing paramaters" };
-    }
-
-    const checkbridgeStatus = await isBridgeRunning();
-
-    console.log("checkbridgeStatus: ", checkbridgeStatus);
-
-    if (!checkbridgeStatus.isSuccess) {
-      return { isSuccess: false, response: "Please connect to bridge" };
-    }
-
-    let response = await axios.put(`${BASE_URI}/play`, args);
-
-    return { isSuccess: true, response: response.data };
-  } catch (error) {
-    console.log("Error: ", error);
-    return { isSuccess: false, error };
+  // validation
+  if (!args.looking_glass_device_index || !args.source) {
+    return { isSuccess: false, message: "Missing paramaters" };
   }
+
+  const checkbridgeStatus = await isBridgeRunning();
+  console.log("checkbridgeStatus: ", checkbridgeStatus);
+
+  if (!checkbridgeStatus) {
+    return { isSuccess: false, message: "Please connect to bridge" };
+  }
+
+  return await bridgeApi({ method: "put", path: "play", parameters: args });
 }
+
 
 export async function encodeYuv420p(args: Yuv420pInput) {
-  try {
-    // validation
-    if (
-      !args.calibration ||
-      !args.source ||
-      !args.destination ||
-      !args.encoder ||
-      !args.crf ||
-      !args.show_window ||
-      !args.scale_x ||
-      !args.scale_y
-    ) {
-      return { isSuccess: false, message: "Missing paramaters" };
-    }
-
-    const checkbridgeStatus = await isBridgeRunning();
-
-    console.log("checkbridgeStatus: ", checkbridgeStatus);
-
-    if (!checkbridgeStatus.isSuccess) {
-      return { isSuccess: false, response: "Please connect to bridge" };
-    }
-
-    let response = await axios.put(`${BASE_URI}/encode`, args);
-
-    return { isSuccess: true, response: response.data };
-  } catch (error) {
-    console.log("Error: ", error);
-    return { isSuccess: false, error };
+  // validation
+  if (
+    !args.calibration ||
+    !args.source ||
+    !args.destination ||
+    !args.encoder ||
+    !args.crf ||
+    !args.show_window ||
+    !args.scale_x ||
+    !args.scale_y
+  ) {
+    return { isSuccess: false, message: "Missing paramaters" };
   }
+
+  const checkbridgeStatus = await isBridgeRunning();
+  console.log("checkbridgeStatus: ", checkbridgeStatus);
+
+  if (!checkbridgeStatus.isSuccess) {
+    return { isSuccess: false, message: "Please connect to bridge" };
+  }
+
+  return await bridgeApi({ method: "put", path: "encode", parameters: args });
+
 }
 
+
 export async function encodeYuv444p(args: Yuv444pInput) {
-  try {
-    // validation
-    if (
-      !args.calibration ||
-      !args.source ||
-      !args.destination ||
-      !args.encoder ||
-      !args.crf ||
-      !args.pixel_format
-    ) {
-      return { isSuccess: false, message: "Missing paramaters" };
-    }
-
-    const checkbridgeStatus = await isBridgeRunning();
-
-    console.log("checkbridgeStatus: ", checkbridgeStatus);
-
-    if (!checkbridgeStatus.isSuccess) {
-      return { isSuccess: false, response: "Please connect to bridge" };
-    }
-
-    let response = await axios.put(`${BASE_URI}/encode`, args);
-
-    return { isSuccess: true, response: response.data };
-  } catch (error) {
-    console.log("Error: ", error);
-    return { isSuccess: false, error };
+  // validation
+  if (
+    !args.calibration ||
+    !args.source ||
+    !args.destination ||
+    !args.encoder ||
+    !args.crf ||
+    !args.pixel_format
+  ) {
+    return { isSuccess: false, message: "Missing paramaters" };
   }
+
+  const checkbridgeStatus = await isBridgeRunning();
+  console.log("checkbridgeStatus: ", checkbridgeStatus);
+
+  if (!checkbridgeStatus.isSuccess) {
+    return { isSuccess: false, message: "Please connect to bridge" };
+  }
+
+  return await bridgeApi({ method: "put", path: "encode", parameters: args });
 }
