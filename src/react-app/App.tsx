1ee6c579b655b378114501b26df5f0a7300f8873
Bryan Brown <BryanChristopherBrown@gmail.com>
2023-06-28 12:39:49 -0400

    move create hologram form to its own component

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -3,12 +3,12 @@ import {
 	BridgeClient,
 	QuiltHologram,
 	RGBDHologram,
-	HologramFactory,
 	PlaylistDeleteMessageHandler,
 	PlaylistInsertMessageHandler,
 	PlaylistInstanceMessageHandler,
 	hologramTypes,
 } from "@library/index"
+import HologramForm from "./components/HologramForm"
 
 const quilt = new QuiltHologram({
 	uri: "https://s3.amazonaws.com/lkg-blocks/u/9aa4b54a7346471d/steampunk_qs8x13.jpg",
@@ -34,7 +34,7 @@ function App() {
 	const [connected, setConnected] = useState<boolean>(false)
 	const [connectionStatus, setConnectionStatus] = useState<string>("🛑 Unable to Connect")
 	const [displays, setDisplays] = useState<string>("checking display status...")
-	
+
 	//internal application state
 	const [isWindowVisible, setIsWindowVisible] = useState(true)
 	const [playlist, setPlaylist] = useState<string>()
@@ -42,14 +42,11 @@ function App() {
 	// Custom Hologram State
 	const [hologram, setHologram] = useState<QuiltHologram | RGBDHologram>(quilt)
 	const [hologramType, setHologramType] = useState<hologramTypes>("quilt")
-	const [hologramUri, setHologramUri] = useState<string>(hologram.uri)
-	const [hologramSettings, setHologramSettings] = useState(hologram.settings)
 
 	// State for managing events and responses from Bridge:
 	const [eventStatus, setEventStatus] = useState<string>("Listen to Events")
 	const eventsink = useRef<HTMLTextAreaElement>(null)
 	const [bridgeResponse, setResponse] = useState<string | null>(null)
-
 
 	const Bridge = BridgeClient.getInstance()
 
@@ -58,8 +55,7 @@ function App() {
 	useEffect(() => {
 		if (hologramType == "quilt") {
 			setHologram(quilt)
-		}
-		else if (hologramType == "rgbd") {
+		} else if (hologramType == "rgbd") {
 			setHologram(rgbd)
 		}
 	}, [hologram])
@@ -68,8 +64,8 @@ function App() {
 		const handleEventConnected = async () => {
 			setConnected(true)
 			setConnectionStatus("✅ Connected")
-			// Manually call Bridge.displays to query for any connected Looking Glass, 
-			// We need to be connected first in order for this to work. 
+			// Manually call Bridge.displays to query for any connected Looking Glass,
+			// We need to be connected first in order for this to work.
 			await Bridge.displays().then((call) => {
 				if (!call.response || call.response.length == 0) {
 					setDisplays("⚠️ No Displays Detected")
@@ -113,218 +109,125 @@ function App() {
 			<div>
 				<div>
 					<h2>Methods</h2>
-					<hr/>
+					<hr />
 					<div className="flex-container">
-					<button
-						onClick={async () => {
-							let call = await Bridge.connect()
-							setResponse(JSON.stringify(call.response))
-							if (call.success) {
-								setConnected(true)
-								setConnectionStatus("✅ Connected to Bridge")
-							}
-						}}
-						disabled={connected}>
-						Connect to Bridge
-					</button>
-					<button
-						onClick={async () => {
-							let call = await Bridge.disconnect()
-							setResponse(JSON.stringify(call.success))
-						}}
-						disabled={!connected}>
-						Disconnect From Bridge
-					</button>
-					<button
-						onClick={async () => {
-							let call = await Bridge.displays()
-							setResponse(JSON.stringify(call.response))
-						}}
-						disabled={!connected}>
-						Get connected displays
-					</button>
-					<button
-						onClick={async () => {
-							let call = await Bridge.getVersion()
-							setResponse(`Looking Glass Bridge Version: ${JSON.stringify(call.response)}`)
-						}}>
-						Get Bridge version
-					</button>
-					<button
-						onClick={async () => {
-							let call = await Bridge.apiVersion()
-							setResponse(`Looking Glass Bridge API Version: ${JSON.stringify(call.response)}`)
-						}}
-						disabled={!connected}>
-						Get API version
-					</button>
-					<button
-						onClick={async () => {
-							setIsWindowVisible(!isWindowVisible)
-							let call = await Bridge.showWindow(isWindowVisible)
-							setResponse(JSON.stringify(call.response))
-						}}
-						disabled={!connected}>
-						Toggle Window
-					</button>
-					</div>
-					<div className="flex-container">
-					<div>
-					<h2>Casting</h2>
-					<hr/>
-					<h3>Create a Hologram</h3>
-					<form>
-						<label>
-						Hologram Type: 
-						<select name="hologramType" id="hologramType"  
-							onChange={ (e) => {
-								setHologramType(e.target.value as hologramTypes)
+						<button
+							onClick={async () => {
+								let call = await Bridge.connect()
+								setResponse(JSON.stringify(call.response))
+								if (call.success) {
+									setConnected(true)
+									setConnectionStatus("✅ Connected to Bridge")
+								}
+							}}
+							disabled={connected}>
+							Connect to Bridge
+						</button>
+						<button
+							onClick={async () => {
+								let call = await Bridge.disconnect()
+								setResponse(JSON.stringify(call.success))
+							}}
+							disabled={!connected}>
+							Disconnect From Bridge
+						</button>
+						<button
+							onClick={async () => {
+								let call = await Bridge.displays()
+								setResponse(JSON.stringify(call.response))
+							}}
+							disabled={!connected}>
+							Get connected displays
+						</button>
+						<button
+							onClick={async () => {
+								let call = await Bridge.getVersion()
+								setResponse(`Looking Glass Bridge Version: ${JSON.stringify(call.response)}`)
 							}}>
-							<option value="quilt">Quilt</option>
-							<option value="rgbd">RGBD</option>
-						</select>
-						</label>
-						<div>
-						<label>
-						uri:
-						<input type="text" onChange={(e) => {
-							setHologramUri(e.target.value)
-						}}></input>
-						</label>
-						</div>
-						{hologramType == "rgbd" && (
-							<div>
-								<h4>RGBD Settings</h4>
-								<label>
-									Depthiness:
-									<input type="number" onChange={(e) => {
-										setHologramSettings({...hologramSettings, depthiness: parseFloat(e.target.value)})
-									}}>
-									</input>
-								</label>
-								<label>
-									Depth Location:
-									<select>
-										<option value="0">Top</option>
-										<option value="1">Bottom</option>
-										<option value="2">Left</option>
-										<option value="3">Right</option>
-									</select>
-								</label>
-								<label>
-									Zoom:
-									<input type="number" onChange={(e) => {
-										setHologramSettings({...hologramSettings, zoom: parseFloat(e.target.value)})
-									}}>
-									</input>
-								</label>
-							</div>
-						)}
-						{hologramType == "quilt" && (
-						<div>
-							<h4>
-								Quilt Settings
-							</h4>
-							<div>
-								<label>
-									Rows:
-									<input type="number"></input>
-								</label>
-							</div>
-							<div>
-								<label>
-									Columns:
-									<input type="number"></input>
-								</label>
-							</div>
-								<label>
-									Aspect Ratio:
-									<input type="number"></input>
-								</label>
-							<div>
-								<label>
-									ViewCount:
-									<input type="number"></input>
-								</label>
-							</div>
-						</div>
-						)}
-				
-					</form>
-					
-					<button
-						onClick={async () => {
-							setResponse("Casting Hologram")
-							setHologramSettings({
-								rows: 8, 
-								columns: 13,
-								aspect: 0.75,
-								viewCount: 8 * 13
-							})
-							let hologram = HologramFactory({
-								uri: "https://s3.amazonaws.com/lkg-blocks/u/9aa4b54a7346471d/steampunk_qs8x13.jpg", 
-								type: hologramType,
-								settings: hologramSettings
-							})
-							setHologram(hologram)
-							let call = await Bridge.cast(hologram)
-							setResponse(JSON.stringify(call))
-							setPlaylist(JSON.stringify(Bridge.playlists))
-						}}
-						disabled={!connected}>
-						Cast hologram
-					</button>
-						<h3>Cast Predefined Holograms</h3>
-					<button
-						title={"Cast a prebuilt quilt hologram"}
-						onClick={async () => {
-							setResponse("Casting Hologram")
-							let call = await Bridge.cast(quilt)
-							setHologram(quilt)
-							setResponse(JSON.stringify(call))
-							setPlaylist(JSON.stringify(Bridge.playlists))
-						}}
-						disabled={!connected}>
-						Cast Quilt hologram
-					</button>
-					<button
-						onClick={async () => {
-							setResponse("Casting Hologram")
-							let call = await Bridge.cast(rgbd)
-							setHologram(rgbd)
-							setResponse(JSON.stringify(call))
-							setPlaylist(JSON.stringify(Bridge.playlists))
-						}}
-						disabled={!connected}>
-						Cast RGBD hologram
-					</button>
-				
+							Get Bridge version
+						</button>
+						<button
+							onClick={async () => {
+								let call = await Bridge.apiVersion()
+								setResponse(`Looking Glass Bridge API Version: ${JSON.stringify(call.response)}`)
+							}}
+							disabled={!connected}>
+							Get API version
+						</button>
+						<button
+							onClick={async () => {
+								setIsWindowVisible(!isWindowVisible)
+								let call = await Bridge.showWindow(isWindowVisible)
+								setResponse(JSON.stringify(call.response))
+							}}
+							disabled={!connected}>
+							Toggle Window
+						</button>
 					</div>
-					{/* PLAYLIST SECTION */}
-					<div>
-						<h2>Properties</h2>
-						<hr/>
-						<h3>Current Hologram:</h3>
-						<p>{JSON.stringify(hologram)}</p>
-						<h3>Playlists:</h3>
-						<p>{playlist}</p>
+
+					<div className="flex-container">
+						<div>
+							<h2>Casting</h2>
+							<hr />
+							<HologramForm
+								connected={connected}
+								hologram={hologram}
+								setHologram={setHologram}
+								hologramType={hologramType}
+								setHologramType={setHologramType}
+								setResponse={setResponse}
+								setPlaylist={setPlaylist}
+							/>
+							<h3>Cast Predefined Holograms</h3>
+							<button
+								title={"Cast a prebuilt quilt hologram"}
+								onClick={async () => {
+									setResponse("Casting Hologram")
+									let call = await Bridge.cast(quilt)
+									setHologram(quilt)
+									setResponse(JSON.stringify(call))
+									setPlaylist(JSON.stringify(Bridge.playlists))
+								}}
+								disabled={!connected}>
+								Cast Quilt hologram
+							</button>
+							<button
+								onClick={async () => {
+									setResponse("Casting Hologram")
+									let call = await Bridge.cast(rgbd)
+									setHologram(rgbd)
+									setResponse(JSON.stringify(call))
+									setPlaylist(JSON.stringify(Bridge.playlists))
+								}}
+								disabled={!connected}>
+								Cast RGBD hologram
+							</button>
+						</div>
+						{/* PLAYLIST SECTION */}
+						<div>
+							<h2>Properties</h2>
+							<hr />
+							<h3>Current Hologram:</h3>
+							<p>{JSON.stringify(hologram)}</p>
+							<h3>Playlists:</h3>
+							<p>{playlist}</p>
+						</div>
 					</div>
 				</div>
 			</div>
-			</div>
 			<h2>Response</h2>
-			<hr/>
+			<hr />
 			<p>{bridgeResponse}</p>
 			<h2>Bridge Events</h2>
 			<button
 				onClick={() => {
 					console.log("%c REACT: Listening to Events", "color: #00ff00")
-					// There are two ways to add events. 
+					// There are two ways to add events.
 					// 1. You can add an event listener to the BridgeClient instance.
 					Bridge.addEventListener("Monitor Connect", (event) => {
 						setDisplays(JSON.stringify(event.payload.value))
 					})
-					// 2. You can create a prebuilt MessageHandler Class. 
+					// 2. You can create a prebuilt MessageHandler Class.
 					new PlaylistInsertMessageHandler({ client: Bridge })
 					new PlaylistInstanceMessageHandler({ client: Bridge })
 					new PlaylistDeleteMessageHandler({ client: Bridge })
---
+++ src/react-app/components/HologramForm.tsx
@@ -0,0 +1,152 @@
+import { BridgeClient, HologramFactory, QuiltHologram, RGBDHologram, hologramTypes } from "@library/index"
+import { useState } from "react"
+
+export interface HologramFactoryArgs {
+	connected: boolean
+	hologram: QuiltHologram | RGBDHologram
+	setHologram: (hologram: QuiltHologram | RGBDHologram) => void
+	setResponse: (response: string | null) => void
+	hologramType: hologramTypes
+	setHologramType: (hologramType: hologramTypes) => void
+	setPlaylist: (playlist: string) => void
+}
+export default function HologramForm({
+	connected,
+	hologram,
+	setHologram,
+	setResponse,
+	hologramType,
+	setHologramType,
+	setPlaylist,
+}: HologramFactoryArgs) {
+	const [hologramUri, setHologramUri] = useState<string>(hologram.uri)
+	const [hologramSettings, setHologramSettings] = useState(hologram.settings)
+	const Bridge = BridgeClient.getInstance()
+	return (
+		<>
+			<h3>Create a Hologram</h3>
+			<form>
+				<label>
+					Hologram Type:
+					<select
+						name="hologramType"
+						id="hologramType"
+						onChange={(e) => {
+							setHologramType(e.target.value as hologramTypes)
+						}}>
+						<option value="quilt">Quilt</option>
+						<option value="rgbd">RGBD</option>
+					</select>
+				</label>
+				<div>
+					<label>
+						uri:
+						<input
+							type="text"
+							onChange={(e) => {
+								// remove "" from the uri
+								let cleaned = e.target.value.replace(/"/g, "")
+								setHologramUri(cleaned)
+							}}></input>
+					</label>
+				</div>
+				{hologramType == "rgbd" && (
+					<div>
+						<h4>RGBD Settings</h4>
+						<label>
+							Depthiness:
+							<input
+								type="number"
+								onChange={(e) => {
+									setHologramSettings({ ...hologramSettings, depthiness: parseFloat(e.target.value) })
+								}}></input>
+						</label>
+						<label>
+							Depth Location:
+							<select
+								onChange={(e) => {
+									setHologramSettings({
+										...hologramSettings,
+										depth_loc: parseInt(e.target.value) as 0 | 1 | 2 | 3,
+									})
+								}}>
+								<option value="0">Top</option>
+								<option value="1">Bottom</option>
+								<option value="2">Right</option>
+								<option value="3">Left</option>
+							</select>
+						</label>
+						<label>
+							Zoom:
+							<input
+								type="number"
+								onChange={(e) => {
+									setHologramSettings({ ...hologramSettings, zoom: parseFloat(e.target.value) })
+								}}></input>
+						</label>
+					</div>
+				)}
+				{hologramType == "quilt" && (
+					<div>
+						<h4>Quilt Settings</h4>
+						<div>
+							<label>
+								Columns:
+								<input
+									type="number"
+									onChange={(e) => {
+										setHologramSettings({ ...hologramSettings, columns: parseInt(e.target.value) })
+									}}></input>
+							</label>
+						</div>
+						<div>
+							<label>
+								Rows:
+								<input
+									type="number"
+									onChange={(e) => {
+										setHologramSettings({ ...hologramSettings, rows: parseInt(e.target.value) })
+									}}></input>
+							</label>
+						</div>
+						<div>
+							<label>
+								ViewCount:
+								<input
+									type="number"
+									onChange={(e) => {
+										setHologramSettings({ ...hologramSettings, viewCount: parseInt(e.target.value) })
+									}}></input>
+							</label>
+						</div>
+					</div>
+				)}
+				<label>
+					Aspect Ratio:
+					<input
+						type="number"
+						onChange={(e) => {
+							setHologramSettings({ ...hologramSettings, aspect: parseFloat(e.target.value) })
+						}}></input>
+				</label>
+			</form>
+
+			<button
+				onClick={async () => {
+					setResponse("Casting Hologram")
+					let hologram = HologramFactory({
+						uri: hologramUri,
+						type: hologramType,
+						settings: hologramSettings,
+					})
+					setHologram(hologram)
+					let call = await Bridge.cast(hologram)
+					setResponse(JSON.stringify(call))
+					setPlaylist(JSON.stringify(Bridge.playlists))
+				}}
+				disabled={!connected}>
+				Cast hologram
+			</button>
+		</>
+	)
+}
