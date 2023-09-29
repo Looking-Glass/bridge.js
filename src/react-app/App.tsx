1568e8d51f8005cd6eaa52204103248ec9e668af
Bryan Brown <BryanChristopherBrown@gmail.com>
2023-09-29 13:52:08 -0400

    add monitor connect/disconnect listeners

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -8,6 +8,8 @@ import {
 	PlaylistInstanceMessageHandler,
 	hologramTypes,
 	Playlist,
+	MonitorConnectedMessageHandler,
+	MonitorDisconnectedMessageHandler,
 } from "@library/index"
 import HologramForm from "./components/HologramForm"
 import { PlaylistUI } from "./components/Playlist"
@@ -441,6 +443,8 @@ function App() {
 					new PlaylistInsertMessageHandler({ client: Bridge })
 					new PlaylistInstanceMessageHandler({ client: Bridge })
 					new PlaylistDeleteMessageHandler({ client: Bridge })
+					new MonitorConnectedMessageHandler({ client: Bridge })
+					new MonitorDisconnectedMessageHandler({ client: Bridge })
 					setEventStatus("Listening to Events")
 				}}
 				disabled={!connected}>
