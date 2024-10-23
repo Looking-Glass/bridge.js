ea5a39c07daf35b398a34dd5a2be741064266817
Bryan Brown <BryanChristopherBrown@gmail.com>
2024-10-23 11:03:33 -0400

    fix: issue with build

--- dist/schemas/schema.utils.d.ts
+++
@@ -1,3 +0,0 @@
-import { z } from "zod";
-/**a utility to allow us to have ranges in our schemas */
-export declare const numberRange: (min: number, max: number) => z.ZodEffects<z.ZodNumber, number, number>;
--- dist/utilities/general.utils.d.ts
+++
@@ -1,8 +0,0 @@
-import { BridgeVersion } from "@library/components/types";
-/**
- * Parse the version from Bridge's version string
- * Bridge uses a major, minor, patch, hotfix versioning system
- * @param versionString The version string to parse
- * @returns BridgeVersion
- */
-export declare function parseBridgeVersion(versionString: string): BridgeVersion;
